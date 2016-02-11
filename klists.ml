40ad5c15f958d5ad6c7fe2ac157c015a38f4f070
kosgei <kosgei@college.harvard.edu>
2016-02-10 21:40:51 -0500

    Added take function

--- klists.ml
+++ klists.ml
@@ -6,6 +6,11 @@
 	Ocaml list operations functions.
 *)
 
+(* Sample OCaml list *)
+let l = [23; 34; 65; 76; 45; 76; 1; 0; 344; 754];;
+let empl = [];;
+
+
 (* Recursively calculate the length of a list. Polymorphous. *)
 
 let rec length l =
@@ -14,4 +19,15 @@ let rec length l =
 	|_::t -> 1 + length t;;
 	
 	
+(*Take n elements from a list *)
+let rec take n l = if n < 0 then raise (Invalid_argument "negative n") else
+  match l with
+  | [] -> raise (Invalid_argument "short list")
+  | [a] -> [a]
+  | h::t -> if n > 0 then h:: take (n-1) t else [];;
+
+
+
+
+(*Drop n elements from a list *)
 
