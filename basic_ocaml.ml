90dec2aedad37ebab3a576f9509bb9e0eff837a2
kosgei <kosgei@college.harvard.edu>
2016-03-11 23:37:21 -0500

    Updated list of functions - list operations, partial application of functions, exceptions

--- basic_ocaml.ml
+++ basic_ocaml.ml
@@ -12,22 +12,150 @@ let empl = [];;
 
 
 (* Recursively calculate the length of a list. Polymorphous. *)
-
 let rec length l =
 	match l with 
 	[] -> 0
-	|_::t -> 1 + length t;;
+	|_::t -> 1 + length t;;	
 	
-	
-(*Take n elements from a list *)
-let rec take n l = if n < 0 then raise (Invalid_argument "negative n") else
+(* Recursively take n elements from a list *)
+let rec take n l = if n < 0 then raise (Invalid_argument "Negative n") else
   match l with
   | [] -> raise (Invalid_argument "short list")
   | [a] -> [a]
   | h::t -> if n > 0 then h:: take (n-1) t else [];;
 
 
+(* recursively replace ! with . *)
+let rec calm (lst:char list) : char list =
+    match lst with 
+    | h::t -> if h = '!' then '.'::calm t else h :: calm t
+    | _ -> [];;
+    
+(* clips integers less than 1 to 1 and greater than 10 to 10 *)
+let rec clip (lst:int list) : int list =
+    match lst with
+    | h::t -> 
+      if h < 1 then 1::clip t else if h > 10 then 10::clip t else h::clip t
+    | [] -> [];;
 
+let clip1 (integ:int ) : int =
+    if integ < 1 then 1 else if integ > 10 then 10 else integ;;
+    
+let clip_list (lst:int list) : int list = List.map clip1 lst;;
 
-(*Drop n elements from a list *)
+let clip_list2 (lst:int list) : int list =
+    List.map (fun x -> if x < 1 then 1 else if x > 10 then 10 else x) lst;;
+
+(* applies a function x times to an argument *)
+let rec apply (f:'a -> 'b) (times:int) (argue: 'a) : ('a -> 'b) =
+    if times < 1 then f else apply f (times - 1) (f argue);;
+    
+(* insert taking a function *)
+let rec insert (f:'a-> 'b -> bool) (el:int) (lst:int list) : int list =
+    match lst with
+    | h::t -> if f el h then el::lst else h::insert f el t
+    | [] -> [];;
+
+(* filters elements based on the logical check of the function *)
+let rec filter (f:'a -> bool) (lst:'a list) : 'a list =
+    match lst with
+    | h::t -> if f h then h::filter f t else filter f t
+    | [] -> [];;
+
+(* is true if all elements return true after function application *)
+let rec for_all (f:'a -> bool) (lst: 'a list) : bool =
+    match lst with
+    | h::t -> if f h then for_all f t else false
+    | [] -> true;;
+
+(* maps through list of lists *)
+let rec mapl (f: 'a list -> 'b list) (lst: 'a list list ) : 'b list list =
+    List.map f lst;;
+
+(* gives the smallest positive number from a list else throws an exception  *)
+let smallest (lst:int list) :int =
+    let rec small_inner (l:int list) (track: int) : int =
+        match l with
+        | h::t -> if h < 0 then small_inner t track else small_inner t h
+        | [] -> if track < 0 then raise Not_found else track
+    in
+    small_inner lst (-1);; 
+let smallest_or_zero (lst:int list) : int =
+    try smallest lst with Not_found -> 0;;
+
+(* calculate largest integer smaller than or equal to the sqrt of given int *)
+exception Negative_int of string;;
+let smint (integ:int): int =
+    if integ < 0 then raise (Negative_int "Expected a positive integer") else
+    let sqrt_float = sqrt (float integ) in
+    int_of_float sqrt_float;;
+let smart_smint (integ: int ) :int =
+    try smint integ with Negative_int _ -> 0;;
+
+(* determines the number of different keys in a dictionary *)
+let keys (dictionary: (int * int) list) : int =
+    let rec inner_keys (lst: (int*int) list) (prev_key: int) (k_num: int): int =
+        match lst with
+        | [] -> k_num - 1
+        | (k,v)::t -> if k = prev_key then inner_keys t prev_key k_num else
+            inner_keys t k (k_num + 1)
+    in inner_keys dictionary min_int 0;;
+
+(* replaces a key with a value, raises Not_found if key wasn't found *)
+let rec replace (kv: (int * int)) (dict: (int * int) list) : (int * int) list =
+    let k',v' = kv in
+    match dict with
+    | [] -> raise Not_found
+    | (k,v)::t -> if k = k' then (k,v')::t else (k,v)::replace kv t ;;
+
+(* builds dictionary from a list of keys and a list of values *)
+let rec dict_build (keys:'a list) (values:'b list) : ('a*'b) list =
+    if List.length keys <> List.length values then
+    raise (Invalid_argument "Unmatching number of keys and values") else
+    match keys with
+    | [] -> []
+    | h::t -> match values with
+        | hd::tl -> (h,hd)::dict_build t tl
+        | [] -> raise (Invalid_argument "Less keys");;
+         
+(* inverse gives a list of keys and a list of values from a dictionary *)
+let inverse (dict:('a*'b) list): 'a list * 'b list =
+    let rec inner_inverse 
+    (dct:('a * 'b) list) (fin_list:'a list * 'b list): 'a list * 'b list =
+        match dct with
+        | [] -> ([],[])
+        | (k,v)::t -> match fin_list with
+            | klst,vlst -> (k::klst, v::vlst)
+    in inner_inverse dict ([],[]);;
+        
+(* turns a list of pairs into a dictionary *)
+let pairs_to_dict (lst:('a * 'b) list) : ('a * 'b) list =
+    let rec inner_pair (lst': ('a * 'b) list) (prev:('k*'v)) : ('a*'b) list =
+    match prev with k , v ->
+    match lst' with
+    | [] -> []
+    | (aa,bb)::t -> if aa = k then (k,v)::inner_pair t (aa,bb) else
+        (aa,bb)::inner_pair t (aa,bb)
+    in 
+    match lst with
+    | [] -> raise (Invalid_argument "Empty list")
+    | (a,b)::tail -> (a,b)::inner_pair tail (a,b);;
+
+(* applies a function to elements in lists in a list *)
+let mapl f lstlst = List.map (List.map f) lstlst;;
+
+(* truncates lists to a specific length in a list of lists *)
+let rec termnator count lst = (if count > 0 then 
+    match lst with 
+    | [] -> [] 
+    | h::t -> h::termnator (count - 1) t
+    else []) ;;
+let truncate trunc lstlst = List.map (termnator trunc) lstlst;;
+
+(* returns a list containing the first elements of llists in a list *)
+let first_elem_lst_lst = List.map 
+    (fun a -> match a with 
+    | [] -> raise (Invalid_argument "Empty list element") 
+    | h::_ -> h);;
+
 
